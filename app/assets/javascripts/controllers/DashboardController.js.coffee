27f7bd7496f8615f5b1c43c03e67bef9d17d97b3
GeorgeYan <abcyys123@126.com>
2014-05-13 17:22:17 +0800

    second commit

---
+++ app/assets/javascripts/controllers/DashboardController.js.coffee
@@ -0,0 +1,2 @@
+angular.module('web_book').controller "DashboardController", ($scope, $routeParams, $location) ->
+
---
+++ app/assets/javascripts/templates_controller.rb
@@ -0,0 +1,9 @@
+class TemplatesController < ApplicationController
+  def index
+
+  end
+
+  def template
+    render :template => 'templates/' + params[:path], :layout => nil
+  end
+end
